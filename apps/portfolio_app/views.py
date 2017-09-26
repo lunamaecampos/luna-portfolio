81a1f7e86d84f7d815f9cbd00830361ae81cd1b5
Luna Campos <lunacampos@Lunas-MBP.T-mobile.com>
2017-09-25 19:40:57 -0700

    even more progress

--- apps/portfolio_app/views.py
+++ apps/portfolio_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render (request, "portfolio_app/index.html")
+
+def resume(request):
+    return render (request, "portfolio_app/resume.html")
