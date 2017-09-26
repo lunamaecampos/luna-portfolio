60de6441f7645e7af59e042eab831d9073760e92
Luna Campos <lunacampos@Lunas-MBP.T-mobile.com>
2017-09-25 17:11:40 -0700

    additions plus js work

--- apps/portfolio_app/static/portfolio_app/js/js.js
+++ apps/portfolio_app/static/portfolio_app/js/js.js
@@ -15,14 +15,40 @@ $(function(){
 });
 $(function(){
   var documentEl = $(document),
-      fadeElem = $("#div2");
+      fadeElem = $("#aboutme");
   documentEl.on('scroll', function(){
     var currScrollPos = documentEl.scrollTop();
 
     fadeElem.each(function(){
       var $this = $(this),
         elemOffsetTop = $this.offset().top;
-      if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/150);
+      if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
+    });
+  });
+});
+$(function(){
+  var documentEl = $(document),
+      fadeElem = $("#div3");
+  documentEl.on('scroll', function(){
+    var currScrollPos = documentEl.scrollTop();
+
+    fadeElem.each(function(){
+      var $this = $(this),
+        elemOffsetTop = $this.offset().top;
+      if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
+    });
+  });
+});
+$(function(){
+  var documentEl = $(document),
+      fadeElem = $("#div4");
+  documentEl.on('scroll', function(){
+    var currScrollPos = documentEl.scrollTop();
+
+    fadeElem.each(function(){
+      var $this = $(this),
+        elemOffsetTop = $this.offset().top;
+      if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
     });
   });
 });
@@ -37,6 +63,52 @@ var part2 = function part2(){
     $('#banner2').delay(400).fadeIn(600);
 });
 }
+// **************************************************************
+// ********************Sliding Animations****************************
+var aboutMeAnimation =function(){
+  $('#aboutme').find('#aboutmeheader').delay(10).animate({opacity:1, left:0},'slow');
+  $('#aboutme').find('#imageholder').delay(500).animate({opacity:1, bottom:0},'slow');
+  $('#aboutme').find('#bio').delay(1000).animate({opacity:1, right:0},'slow');
+}
+$(function(){
+  var documentEl = $(document),
+      fadeElem = $("#aboutme");
+  documentEl.on('scroll', function(){
+    var currScrollPos = documentEl.scrollTop();
+
+    fadeElem.each(function(){
+      var $this = $(this),
+        elemOffsetTop = $this.offset().top;
+      if(currScrollPos > elemOffsetTop - 500 || currScrollPos == elemOffsetTop){
+        aboutMeAnimation();
+      }
+    });
+  });
+});
+
+var techAnimation =function(){
+  $('#techdiv').find('.banner').delay(10).animate({opacity:1, left:0},'slow');
+  $('#techdiv').find('.assetmenumain').delay(500).animate({opacity:1, bottom:0},'slow');
+  $('#techdiv').find('#assetrow1').delay(1000).animate({opacity:1, left:0},'slow');
+  $('#techdiv').find('#assetrow2').delay(1500).animate({opacity:1, right:0},'slow');
+  $('#techdiv').find('#assetrow3').delay(2000).animate({opacity:1, left:0},'slow');
+}
+$(function(){
+  var documentEl = $(document),
+      fadeElem = $("#techdiv");
+  documentEl.on('scroll', function(){
+    var currScrollPos = documentEl.scrollTop();
+
+    fadeElem.each(function(){
+      var $this = $(this),
+        elemOffsetTop = $this.offset().top;
+      if(currScrollPos > elemOffsetTop - 500 || currScrollPos == elemOffsetTop){
+        techAnimation();
+      }
+    });
+  });
+});
+
 // **************************************************************
 
 // ********************Landing carousel****************************
