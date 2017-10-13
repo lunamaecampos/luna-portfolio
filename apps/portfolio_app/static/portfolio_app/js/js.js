83b44527c2b052e0e07625c0888d9d1b8434a680
Luna Campos <lunacampos@Lunas-MBP.T-mobile.com>
2017-10-12 18:31:31 -0700

    some new updates

--- apps/portfolio_app/static/portfolio_app/js/js.js
+++ apps/portfolio_app/static/portfolio_app/js/js.js
@@ -35,7 +35,7 @@ $(function(){
     fadeElem.each(function(){
       var $this = $(this),
         elemOffsetTop = $this.offset().top;
-      if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
+      if(currScrollPos > elemOffsetTop ) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
     });
   });
 });
@@ -48,7 +48,7 @@ $(function(){
     fadeElem.each(function(){
       var $this = $(this),
         elemOffsetTop = $this.offset().top;
-      if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
+      if(currScrollPos > elemOffsetTop ) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
     });
   });
 });
@@ -110,6 +110,7 @@ $(function(){
 });
 var projectsAnimation =function(){
   $('#projectsdiv').find('.banner').delay(10).animate({opacity:1, right:0},'slow');
+  $('#projectsdiv').find('#projectthumbnails').delay(500).animate({opacity:1, top:0},'slow');
 }
 $(function(){
   var documentEl = $(document),
@@ -126,6 +127,66 @@ $(function(){
     });
   });
 });
+// **************************************************************
+
+// ********************Thumbnails****************************
+// var hideThumbs = function(){
+//   $(document).ready(function(){
+//       $("#LSEMENU").click(function(){
+//           $('.bgdescthumb').hide('slow');
+//           // $('#lsesynththumbs').find('.bgdescthumb').delay(1000).show('slow');
+//       });
+//   });
+// }
+// hideThumbs();
+$(document).ready(function(){
+    // $('.descthumbs').css('display', 'none')
+
+    $("#LSEMENU").click(function(){
+        $('.bgdescthumb').hide('slow', function(){
+          $('#lsesynththumbs').find('.bgdescthumb').delay(400).show('slow').css('display', 'inline-block');
+        });
+        $('.projectinfoleft').hide('slow', function(){
+          $('#lsesynthinfo').find('.projectinfoleft').delay(400).show('slow').css('display', 'inline-block');
+        });
+    });
+
+    $("#XINXINMENU").click(function(){
+        $('.bgdescthumb').hide('slow', function(){
+          $('#xinxinthumbs').find('.bgdescthumb').delay(400).show('slow').css('display', 'inline-block');
+        });
+        $('.projectinfoleft').hide('slow', function(){
+          $('#xinxininfo').find('.projectinfoleft').delay(400).show('slow').css('display', 'inline-block');
+        });
+    });
+
+    $("#GIROMENU").click(function(){
+        $('.bgdescthumb').hide('slow', function(){
+          $('#synthgirothumbs').find('.bgdescthumb').delay(400).show('slow').css('display', 'inline-block');
+        });
+        $('.projectinfoleft').hide('slow', function(){
+          $('#synthgyroinfo').find('.projectinfoleft').delay(400).show('slow').css('display', 'inline-block');
+        });
+    });
+
+    $("#TASKLISTMENU").click(function(){
+        $('.bgdescthumb').hide('slow', function(){
+          $('#taskthumbs').find('.bgdescthumb').delay(400).show('slow').css('display', 'inline-block');
+        });
+        $('.projectinfoleft').hide('slow', function(){
+          $('#taskthumbsinfo').find('.projectinfoleft').delay(400).show('slow').css('display', 'inline-block');
+        });
+    });
+    $("#BLOSMMENU").click(function(){
+        $('.bgdescthumb').hide('slow', function(){
+          $('#blosmthumbs').find('.bgdescthumb').delay(400).show('slow').css('display', 'inline-block');
+        });
+        $('.projectinfoleft').hide('slow', function(){
+          $('#blosminfo').find('.projectinfoleft').delay(400).show('slow').css('display', 'inline-block');
+        });
+    });
+});
+
 // **************************************************************
 
 // ********************Landing carousel****************************
@@ -154,4 +215,45 @@ $('.carousel').carousel({
   interval: 1000,
   pause: "false"
 });
+// **************************************************************
+
+// ********************Modal Gallery****************************
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
 // *********************************************************************
+// ********************Parallax Scrolling******************************
