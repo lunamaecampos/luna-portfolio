507d1dc051a61ca7b9c2400f25a5d9b7fb5154cd
Luna Campos <lunacampos@Lunas-MBP.T-mobile.com>
2017-10-13 18:20:13 -0700

    now responsive for mobile

--- apps/portfolio_app/static/portfolio_app/js/js.js
+++ apps/portfolio_app/static/portfolio_app/js/js.js
@@ -1,57 +1,57 @@
 // *********Fade out for pages***********************
-
-$(function(){
-  var documentEl = $(document),
-      fadeElem = $(".slide");
-  documentEl.on('scroll', function(){
-    var currScrollPos = documentEl.scrollTop();
-
-    fadeElem.each(function(){
-      var $this = $(this),
-        elemOffsetTop = $this.offset().top;
-      if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/500);
-    });
-  });
-});
-$(function(){
-  var documentEl = $(document),
-      fadeElem = $("#aboutme");
-  documentEl.on('scroll', function(){
-    var currScrollPos = documentEl.scrollTop();
-
-    fadeElem.each(function(){
-      var $this = $(this),
-        elemOffsetTop = $this.offset().top;
-      if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
-    });
-  });
-});
-$(function(){
-  var documentEl = $(document),
-      fadeElem = $("#projectsdiv");
-  documentEl.on('scroll', function(){
-    var currScrollPos = documentEl.scrollTop();
-
-    fadeElem.each(function(){
-      var $this = $(this),
-        elemOffsetTop = $this.offset().top;
-      if(currScrollPos > elemOffsetTop ) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
-    });
-  });
-});
-$(function(){
-  var documentEl = $(document),
-      fadeElem = $("#div4");
-  documentEl.on('scroll', function(){
-    var currScrollPos = documentEl.scrollTop();
-
-    fadeElem.each(function(){
-      var $this = $(this),
-        elemOffsetTop = $this.offset().top;
-      if(currScrollPos > elemOffsetTop ) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
-    });
-  });
-});
+// 
+// $(function(){
+//   var documentEl = $(document),
+//       fadeElem = $(".slide");
+//   documentEl.on('scroll', function(){
+//     var currScrollPos = documentEl.scrollTop();
+//
+//     fadeElem.each(function(){
+//       var $this = $(this),
+//         elemOffsetTop = $this.offset().top;
+//       if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/500);
+//     });
+//   });
+// });
+// $(function(){
+//   var documentEl = $(document),
+//       fadeElem = $("#aboutme");
+//   documentEl.on('scroll', function(){
+//     var currScrollPos = documentEl.scrollTop();
+//
+//     fadeElem.each(function(){
+//       var $this = $(this),
+//         elemOffsetTop = $this.offset().top;
+//       if(currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
+//     });
+//   });
+// });
+// $(function(){
+//   var documentEl = $(document),
+//       fadeElem = $("#projectsdiv");
+//   documentEl.on('scroll', function(){
+//     var currScrollPos = documentEl.scrollTop();
+//
+//     fadeElem.each(function(){
+//       var $this = $(this),
+//         elemOffsetTop = $this.offset().top;
+//       if(currScrollPos > elemOffsetTop ) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
+//     });
+//   });
+// });
+// $(function(){
+//   var documentEl = $(document),
+//       fadeElem = $("#div4");
+//   documentEl.on('scroll', function(){
+//     var currScrollPos = documentEl.scrollTop();
+//
+//     fadeElem.each(function(){
+//       var $this = $(this),
+//         elemOffsetTop = $this.offset().top;
+//       if(currScrollPos > elemOffsetTop ) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
+//     });
+//   });
+// });
 // **************************************************************
 
 // ********************Carousel Text Fading****************************
